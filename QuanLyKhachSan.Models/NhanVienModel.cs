d09c8cad572fd7bb572d8bff483a03aa9f400b90
NgoXCuong <ngocuongcm2005@gmail.com>
2025-05-10 12:58:55 +0700

    Thêm logic NhanVien

---
+++ QuanLyKhachSan.Models/NhanVienModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.Models
+{
+    public class NhanVienModel
+    {
+        public int Id { get; set; }
+        public string HoTen { get; set; }
+        public string GioiTinh { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string ChucVu { get; set; }
+        public string SoDienThoai { get; set; }
+        public string Email { get; set; }
+    }
+}
